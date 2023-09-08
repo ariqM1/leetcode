f8c8d00eb53fa998cc8d33721569b60a922ed17e
Ariq Mukul <ariqmukul@gmail.com>
2023-09-08 12:53:41 -0400

    9.8.2023

---
+++ jump_game_II.py
@@ -0,0 +1,17 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        l, r = 0, 0
+        steps = 0
+
+        while r < (len(nums)-1):
+            maxJump = 0
+            for i in range(l, r+1):
+                maxJump = max(maxJump, nums[i] + i)
+            l = r+1
+            r = maxJump
+            steps += 1
+        
+
+        return steps
+
+        
